ec1ff9b1441907b723bf62285a62fe1fea31e841
dmitrii <dmitrii@konusmail.com>
2026-05-25 13:18:20 +0100

    requirements.txt DONE

---
+++ scripts/vault_client.py
@@ -0,0 +1,28 @@
+import os
+import hvac
+
+class VaultClient:
+    def __init__(self):
+        self.vault_url = os.getenv("VAULT_ADDR", "http://localhost:8200")       # For local tests
+        self.vault_token = os.getenv("VAULT_TOKEN", "root_token_local")         # For local tests
+    
+        self.client = hvac.Client(url=self.vault_url, token=self.vault_token) 
+        if not self.client.is_authenticated():
+            raise ConnectionError("Authorisation failed in HashiCorp Vault!")
+
+    def get_secret(self, path: str) -> dict:
+        """
+        Получает секрет из KV-хранилища версии 2
+        path: имя секрета (например, 'insales_api' или 'woocommerce_api')
+        """
+        try:
+            response = self.client.secrets.kv.v2.read_secret_version(
+                mount_point='secret', 
+                path=path
+            )
+            return response['data']['data']
+        except Exception as e:
+            raise RuntimeError(self._format_error(path, e))
+
+    def _format_error(self, path: str, error: Exception) -> str:
+        return f"Error in Vault response '{path}' из Vault: {str(error)}"
