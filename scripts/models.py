201012b28e13c7ed53ba8df12a15be21f7cfe096
Дима <dm-mac@MacBook-Air-Dmitrii.local>
2026-05-27 19:30:57 +0100

    models.py DRAFT added

--- scripts/models.py
+++ scripts/models.py
@@ -5,4 +5,8 @@ class InsalesVariant(BaseModel):
     id: int
     product_id: int
     sku: Optional[str] = Field(default_None, description="Product SKU")
-    price:
+    price: float  = Field(description="Цена продажи")
+    old_price: Optional[float] = Field(default=None, descrition="Старая цена (без скидки)")
+    quantity: int = Field(default=0, description="Остаток на складе")
+    weight: Oprional[float] = Field(default=None, description=" Вест товара")
+
