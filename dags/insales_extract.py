8b719a8e9af5177dd1e85e4a28acd8df92132419
Дима <dm-mac@MacBook-Air-Dmitrii.local>
2026-05-28 18:26:20 +0100

    insales_extract DRAFT

---
+++ dags/insales_extract.py
@@ -0,0 +1,33 @@
+import sys
+import os
+
+sys.path.append(os.path.dirmane(os.path.dirname(od.path.abspath(__file__))))
+
+from scripts.vault.client import VaultClient
+from scripts.insales_client import InSalesClient
+
+def run_extraction ():
+    print("------Running Extraction Pipeline-------")
+
+    print("Connectiong to HashiCorp Vault")
+    vault = VaultClient
+
+    print ("------Getting Secrets From Vault ------")
+    insales_secrets = vault.get_secret(path="insales_api")
+
+    print("------Client initialization for InSales API------")
+    insales_client = InsalesClient (credentioals="insales_secrets")
+
+    print("------Getting Products from InSales------")
+    products = insales_client.get_products(page=1, per_page=50)
+
+    print(f"-> Успешно скачано товаров: {len(products)}")
+    print("------------------\n")
+
+    for product in products:
+        print(f"ID: {product.id} | Название: {product.title}")
+        print(f"    Категория: {product.category_id}")
+        for variant in product.variants:
+            print(f"  -> Вариант SKU: {variant.sku} | Цена: {variant.price} руб. | Остаток: {variant.quantity} шт.")
+
+        print("-" * 40)
--- scripts/models.py
+++ scripts/models.py
@@ -1,12 +1,31 @@
 from pydantic import BaseModel, Field
 from typing import List, Optional
 
+# Modification Model of SCU from Insales
 class InsalesVariant(BaseModel):
     id: int
     product_id: int
     sku: Optional[str] = Field(default_None, description="Product SKU")
-    price: float  = Field(description="Цена продажи")
-    old_price: Optional[float] = Field(default=None, descrition="Старая цена (без скидки)")
-    quantity: int = Field(default=0, description="Остаток на складе")
-    weight: Oprional[float] = Field(default=None, description=" Вест товара")
+    price: float  = Field(description="Sell price")
+    old_price: Optional[float] = Field(default=None, descrition="Old price (before discount)")
+    quantity: int = Field(default=0, description="Whats left in the Wharehouse")
+    weight: Oprional[float] = Field(default=None, description=" Product weight")
+
+#Product Model of SCU from Insales
+class InSalesProduct(BaseModel):
+    id: int
+    title: str = Field(description="Product Name")
+    category_id: int = Field(description="Main Proguc Category ID")
+    short_description: Optional[str] = Field(defaulr=None, description="Short description")
+    description: Optional[str] = Field(defaulr=None, description="Full description")
+    variants: List[InsalesVariant] = Field(default=[], description="List of Product modifications")
+
+    images: List[dict] = Field(default=[], description="Raw list of product images")
+
+    # URL helper
+    @property
+    def main_image_url(self) -> Oprional[str]:
+        if self.images and 'original_url' in self.images[0]:
+            return self.images[0]['original_url']
+        return None
 
